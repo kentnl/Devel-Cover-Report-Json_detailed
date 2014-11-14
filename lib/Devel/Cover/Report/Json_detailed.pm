eeccda1b046f74b15d97b9293b309db4a17f04a0
Kent Fredric <kentfredric@gmail.com>
2014-11-14 13:41:23 +1300

    initial commit

---
+++ lib/Devel/Cover/Report/Json_detailed.pm
@@ -0,0 +1,18 @@
+use 5.008;    # utf8
+use strict;
+use warnings;
+use utf8;
+
+package Devel::Cover::Report::Json_detailed;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT:
+
+# AUTHORITY
+
+use Moo;
+
+no Moo;
+
+1;
