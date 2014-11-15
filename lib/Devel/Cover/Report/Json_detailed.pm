5a3dd2ce5820390f3bf6c506e2d4ade7e894cf2f
Kent Fredric <kentfredric@gmail.com>
2014-11-16 12:11:50 +1300

    v0.001000
     - First version.

--- lib/Devel/Cover/Report/Json_detailed.pm
+++ lib/Devel/Cover/Report/Json_detailed.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Devel::Cover::Report::Json_detailed;
 
-our $VERSION = '0.001000';
+our $VERSION = '0.001001';
 
 # ABSTRACT: Generate a comprehensive JSON file articulating the full contents of a coverage run.
 
